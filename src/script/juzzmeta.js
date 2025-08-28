db014219e21fa327c96c055169fb41d48f4b7fdc
Hassan Raza <hassan.raza@redmath.com>
2025-08-28 17:31:56 +0500

    audio intro added

---
+++ src/script/juzzmeta.js
@@ -0,0 +1,332 @@
+const juzzmeta = [
+    {
+        "index": "01",
+        "start": {
+            "surah": "001",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "002",
+            "verse": "141"
+        }
+    },
+    {
+        "index": "02",
+        "start": {
+            "surah": "002",
+            "verse": "142"
+        },
+        "end": {
+            "surah": "002",
+            "verse": "252"
+        }
+    },
+    {
+        "index": "03",
+        "start": {
+            "surah": "002",
+            "verse": "253"
+        },
+        "end": {
+            "surah": "003",
+            "verse": "92"
+        }
+    },
+    {
+        "index": "04",
+        "start": {
+            "surah": "003",
+            "verse": "93"
+        },
+        "end": {
+            "surah": "004",
+            "verse": "23"
+        }
+    },
+    {
+        "index": "05",
+        "start": {
+            "surah": "004",
+            "verse": "24"
+        },
+        "end": {
+            "surah": "004",
+            "verse": "147"
+        }
+    },
+    {
+        "index": "06",
+        "start": {
+            "surah": "004",
+            "verse": "148"
+        },
+        "end": {
+            "surah": "005",
+            "verse": "81"
+        }
+    },
+    {
+        "index": "07",
+        "start": {
+            "surah": "005",
+            "verse": "82"
+        },
+        "end": {
+            "surah": "006",
+            "verse": "110"
+        }
+    },
+    {
+        "index": "08",
+        "start": {
+            "surah": "006",
+            "verse": "111"
+        },
+        "end": {
+            "surah": "007",
+            "verse": "87"
+        }
+    },
+    {
+        "index": "09",
+        "start": {
+            "surah": "007",
+            "verse": "88"
+        },
+        "end": {
+            "surah": "008",
+            "verse": "40"
+        }
+    },
+    {
+        "index": "10",
+        "start": {
+            "surah": "008",
+            "verse": "41"
+        },
+        "end": {
+            "surah": "009",
+            "verse": "92"
+        }
+    },
+    {
+        "index": "11",
+        "start": {
+            "surah": "009",
+            "verse": "93"
+        },
+        "end": {
+            "surah": "011",
+            "verse": "5"
+        }
+    },
+    {
+        "index": "12",
+        "start": {
+            "surah": "011",
+            "verse": "6"
+        },
+        "end": {
+            "surah": "012",
+            "verse": "52"
+        }
+    },
+    {
+        "index": "13",
+        "start": {
+            "surah": "012",
+            "verse": "53"
+        },
+        "end": {
+            "surah": "014",
+            "verse": "52"
+        }
+    },
+    {
+        "index": "14",
+        "start": {
+            "surah": "015",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "016",
+            "verse": "128"
+        }
+    },
+    {
+        "index": "15",
+        "start": {
+            "surah": "017",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "018",
+            "verse": "74"
+        }
+    },
+    {
+        "index": "16",
+        "start": {
+            "surah": "018",
+            "verse": "75"
+        },
+        "end": {
+            "surah": "020",
+            "verse": "135"
+        }
+    },
+    {
+        "index": "17",
+        "start": {
+            "surah": "021",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "022",
+            "verse": "78"
+        }
+    },
+    {
+        "index": "18",
+        "start": {
+            "surah": "023",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "025",
+            "verse": "20"
+        }
+    },
+    {
+        "index": "19",
+        "start": {
+            "surah": "025",
+            "verse": "21"
+        },
+        "end": {
+            "surah": "027",
+            "verse": "55"
+        }
+    },
+    {
+        "index": "20",
+        "start": {
+            "surah": "027",
+            "verse": "56"
+        },
+        "end": {
+            "surah": "029",
+            "verse": "45"
+        }
+    },
+    {
+        "index": "21",
+        "start": {
+            "surah": "029",
+            "verse": "46"
+        },
+        "end": {
+            "surah": "033",
+            "verse": "30"
+        }
+    },
+    {
+        "index": "22",
+        "start": {
+            "surah": "033",
+            "verse": "31"
+        },
+        "end": {
+            "surah": "036",
+            "verse": "27"
+        }
+    },
+    {
+        "index": "23",
+        "start": {
+            "surah": "036",
+            "verse": "28"
+        },
+        "end": {
+            "surah": "039",
+            "verse": "31"
+        }
+    },
+    {
+        "index": "24",
+        "start": {
+            "surah": "039",
+            "verse": "32"
+        },
+        "end": {
+            "surah": "041",
+            "verse": "46"
+        }
+    },
+    {
+        "index": "25",
+        "start": {
+            "surah": "041",
+            "verse": "47"
+        },
+        "end": {
+            "surah": "045",
+            "verse": "37"
+        }
+    },
+    {
+        "index": "26",
+        "start": {
+            "surah": "046",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "051",
+            "verse": "30"
+        }
+    },
+    {
+        "index": "27",
+        "start": {
+            "surah": "051",
+            "verse": "31"
+        },
+        "end": {
+            "surah": "057",
+            "verse": "29"
+        }
+    },
+    {
+        "index": "28",
+        "start": {
+            "surah": "058",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "066",
+            "verse": "12"
+        }
+    },
+    {
+        "index": "29",
+        "start": {
+            "surah": "067",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "077",
+            "verse": "50"
+        }
+    },
+    {
+        "index": "30",
+        "start": {
+            "surah": "078",
+            "verse": "1"
+        },
+        "end": {
+            "surah": "114",
+            "verse": "6"
+        }
+    }
+];
