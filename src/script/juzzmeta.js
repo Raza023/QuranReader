03b03d6dd1d77844eee14491fb6e7c850fbce918
Hassan <hr770735@gmail.com>
2025-08-30 22:35:27 +0500

    minor changes in UI

--- src/script/juzzmeta.js
+++ src/script/juzzmeta.js
@@ -1,332 +1,212 @@
 const juzzmeta = [
     {
         "index": "01",
-        "start": {
-            "surah": "001",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "002",
-            "verse": "141"
-        }
+        "start": { "surah": "001", "verse": "1" },
+        "end": { "surah": "002", "verse": "141" },
+        "name": "Alif Lam Mim",
+        "urduname": "الف لام میم"
     },
     {
         "index": "02",
-        "start": {
-            "surah": "002",
-            "verse": "142"
-        },
-        "end": {
-            "surah": "002",
-            "verse": "252"
-        }
+        "start": { "surah": "002", "verse": "142" },
+        "end": { "surah": "002", "verse": "252" },
+        "name": "Sayaqal",
+        "urduname": "سَیَقُولُ"
     },
     {
         "index": "03",
-        "start": {
-            "surah": "002",
-            "verse": "253"
-        },
-        "end": {
-            "surah": "003",
-            "verse": "92"
-        }
+        "start": { "surah": "002", "verse": "253" },
+        "end": { "surah": "003", "verse": "92" },
+        "name": "Tilka'r-Rusulu",
+        "urduname": "تِلْکَ الرُّسُلُ"
     },
     {
         "index": "04",
-        "start": {
-            "surah": "003",
-            "verse": "93"
-        },
-        "end": {
-            "surah": "004",
-            "verse": "23"
-        }
+        "start": { "surah": "003", "verse": "93" },
+        "end": { "surah": "004", "verse": "23" },
+        "name": "Lan Tana Lu",
+        "urduname": "لَنْ تَنَالُوا"
     },
     {
         "index": "05",
-        "start": {
-            "surah": "004",
-            "verse": "24"
-        },
-        "end": {
-            "surah": "004",
-            "verse": "147"
-        }
+        "start": { "surah": "004", "verse": "24" },
+        "end": { "surah": "004", "verse": "147" },
+        "name": "Wal-Muhsanat",
+        "urduname": "وَالْمُحْصَنَاتُ"
     },
     {
         "index": "06",
-        "start": {
-            "surah": "004",
-            "verse": "148"
-        },
-        "end": {
-            "surah": "005",
-            "verse": "81"
-        }
+        "start": { "surah": "004", "verse": "148" },
+        "end": { "surah": "005", "verse": "81" },
+        "name": "La Yuhibbullah",
+        "urduname": "لَا یُحِبُّ اللَّهُ"
     },
     {
         "index": "07",
-        "start": {
-            "surah": "005",
-            "verse": "82"
-        },
-        "end": {
-            "surah": "006",
-            "verse": "110"
-        }
+        "start": { "surah": "005", "verse": "82" },
+        "end": { "surah": "006", "verse": "110" },
+        "name": "Wa Iza Samiu",
+        "urduname": "وَإِذَا سَمِعُوا"
     },
     {
         "index": "08",
-        "start": {
-            "surah": "006",
-            "verse": "111"
-        },
-        "end": {
-            "surah": "007",
-            "verse": "87"
-        }
+        "start": { "surah": "006", "verse": "111" },
+        "end": { "surah": "007", "verse": "87" },
+        "name": "Wa Lau Annana",
+        "urduname": "وَلَوْ أَنَّنَا"
     },
     {
         "index": "09",
-        "start": {
-            "surah": "007",
-            "verse": "88"
-        },
-        "end": {
-            "surah": "008",
-            "verse": "40"
-        }
+        "start": { "surah": "007", "verse": "88" },
+        "end": { "surah": "008", "verse": "40" },
+        "name": "Ya'tadhiru",
+        "urduname": "یَعْتَذِرُونَ"
     },
     {
         "index": "10",
-        "start": {
-            "surah": "008",
-            "verse": "41"
-        },
-        "end": {
-            "surah": "009",
-            "verse": "92"
-        }
+        "start": { "surah": "008", "verse": "41" },
+        "end": { "surah": "009", "verse": "92" },
+        "name": "A'lam Yara",
+        "urduname": "أَلَمْ یَعْلَمْ"
     },
     {
         "index": "11",
-        "start": {
-            "surah": "009",
-            "verse": "93"
-        },
-        "end": {
-            "surah": "011",
-            "verse": "5"
-        }
+        "start": { "surah": "009", "verse": "93" },
+        "end": { "surah": "011", "verse": "5" },
+        "name": "Yunadeena",
+        "urduname": "یُنَادُونَ"
     },
     {
         "index": "12",
-        "start": {
-            "surah": "011",
-            "verse": "6"
-        },
-        "end": {
-            "surah": "012",
-            "verse": "52"
-        }
+        "start": { "surah": "011", "verse": "6" },
+        "end": { "surah": "012", "verse": "52" },
+        "name": "Wa Ma Min",
+        "urduname": "وَمَا مِنْ دَابَّة"
     },
     {
         "index": "13",
-        "start": {
-            "surah": "012",
-            "verse": "53"
-        },
-        "end": {
-            "surah": "014",
-            "verse": "52"
-        }
+        "start": { "surah": "012", "verse": "53" },
+        "end": { "surah": "014", "verse": "52" },
+        "name": "Wa Ma Ubri'u",
+        "urduname": "وَمَا أُبَرِّئُ"
     },
     {
         "index": "14",
-        "start": {
-            "surah": "015",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "016",
-            "verse": "128"
-        }
+        "start": { "surah": "015", "verse": "1" },
+        "end": { "surah": "016", "verse": "128" },
+        "name": "Rubama",
+        "urduname": "رُبَمَا"
     },
     {
         "index": "15",
-        "start": {
-            "surah": "017",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "018",
-            "verse": "74"
-        }
+        "start": { "surah": "017", "verse": "1" },
+        "end": { "surah": "018", "verse": "74" },
+        "name": "Subhanallazi",
+        "urduname": "سُبْحَانَ الَّذِی"
     },
     {
         "index": "16",
-        "start": {
-            "surah": "018",
-            "verse": "75"
-        },
-        "end": {
-            "surah": "020",
-            "verse": "135"
-        }
+        "start": { "surah": "018", "verse": "75" },
+        "end": { "surah": "020", "verse": "135" },
+        "name": "Qal Alam",
+        "urduname": "قَالَ أَلَمْ"
     },
     {
         "index": "17",
-        "start": {
-            "surah": "021",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "022",
-            "verse": "78"
-        }
+        "start": { "surah": "021", "verse": "1" },
+        "end": { "surah": "022", "verse": "78" },
+        "name": "Aqtarab",
+        "urduname": "اقْتَرَبَ"
     },
     {
         "index": "18",
-        "start": {
-            "surah": "023",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "025",
-            "verse": "20"
-        }
+        "start": { "surah": "023", "verse": "1" },
+        "end": { "surah": "025", "verse": "20" },
+        "name": "Qad Aflaha",
+        "urduname": "قَدْ أَفْلَحَ"
     },
     {
         "index": "19",
-        "start": {
-            "surah": "025",
-            "verse": "21"
-        },
-        "end": {
-            "surah": "027",
-            "verse": "55"
-        }
+        "start": { "surah": "025", "verse": "21" },
+        "end": { "surah": "027", "verse": "55" },
+        "name": "Wa Qalallazina",
+        "urduname": "وَقَالَ الَّذِینَ"
     },
     {
         "index": "20",
-        "start": {
-            "surah": "027",
-            "verse": "56"
-        },
-        "end": {
-            "surah": "029",
-            "verse": "45"
-        }
+        "start": { "surah": "027", "verse": "56" },
+        "end": { "surah": "029", "verse": "45" },
+        "name": "Qala Alam",
+        "urduname": "قَالَ أَلَمْ"
     },
     {
         "index": "21",
-        "start": {
-            "surah": "029",
-            "verse": "46"
-        },
-        "end": {
-            "surah": "033",
-            "verse": "30"
-        }
+        "start": { "surah": "029", "verse": "46" },
+        "end": { "surah": "033", "verse": "30" },
+        "name": "Utlu Ma",
+        "urduname": "اتْلُ مَا"
     },
     {
         "index": "22",
-        "start": {
-            "surah": "033",
-            "verse": "31"
-        },
-        "end": {
-            "surah": "036",
-            "verse": "27"
-        }
+        "start": { "surah": "033", "verse": "31" },
+        "end": { "surah": "036", "verse": "27" },
+        "name": "Wa Manyaqnut",
+        "urduname": "وَمَن یَقْنُتْ"
     },
     {
         "index": "23",
-        "start": {
-            "surah": "036",
-            "verse": "28"
-        },
-        "end": {
-            "surah": "039",
-            "verse": "31"
-        }
+        "start": { "surah": "036", "verse": "28" },
+        "end": { "surah": "039", "verse": "31" },
+        "name": "Faman Azlam",
+        "urduname": "فَمَنْ أَظْلَمُ"
     },
     {
         "index": "24",
-        "start": {
-            "surah": "039",
-            "verse": "32"
-        },
-        "end": {
-            "surah": "041",
-            "verse": "46"
-        }
+        "start": { "surah": "039", "verse": "32" },
+        "end": { "surah": "041", "verse": "46" },
+        "name": "Elaihi Yurad",
+        "urduname": "إِلَیْهِ یُرَدُّ"
     },
     {
         "index": "25",
-        "start": {
-            "surah": "041",
-            "verse": "47"
-        },
-        "end": {
-            "surah": "045",
-            "verse": "37"
-        }
+        "start": { "surah": "041", "verse": "47" },
+        "end": { "surah": "045", "verse": "37" },
+        "name": "Ha'a Meem",
+        "urduname": "حٰم"
     },
     {
         "index": "26",
-        "start": {
-            "surah": "046",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "051",
-            "verse": "30"
-        }
+        "start": { "surah": "046", "verse": "1" },
+        "end": { "surah": "051", "verse": "30" },
+        "name": "Khalaq",
+        "urduname": "خَلَقَ"
     },
     {
         "index": "27",
-        "start": {
-            "surah": "051",
-            "verse": "31"
-        },
-        "end": {
-            "surah": "057",
-            "verse": "29"
-        }
+        "start": { "surah": "051", "verse": "31" },
+        "end": { "surah": "057", "verse": "29" },
+        "name": "Qala Fama",
+        "urduname": "قَالَ فَمَا"
     },
     {
         "index": "28",
-        "start": {
-            "surah": "058",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "066",
-            "verse": "12"
-        }
+        "start": { "surah": "058", "verse": "1" },
+        "end": { "surah": "066", "verse": "12" },
+        "name": "Tabarakallazi",
+        "urduname": "تَبَارَکَ الَّذِی"
     },
     {
         "index": "29",
-        "start": {
-            "surah": "067",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "077",
-            "verse": "50"
-        }
+        "start": { "surah": "067", "verse": "1" },
+        "end": { "surah": "077", "verse": "50" },
+        "name": "Amma",
+        "urduname": "عَمَّ"
     },
     {
         "index": "30",
-        "start": {
-            "surah": "078",
-            "verse": "1"
-        },
-        "end": {
-            "surah": "114",
-            "verse": "6"
-        }
+        "start": { "surah": "078", "verse": "1" },
+        "end": { "surah": "114", "verse": "6" },
+        "name": "Qad Sami",
+        "urduname": "قَدْ سَمِعَ"
     }
 ];
